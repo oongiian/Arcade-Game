f17b69a30bd94909fde295eade12b72d7b2a7054
dongxian <dongxianchan@gmail.com>
2018-04-02 23:28:50 +0800

    基本功能完成，准备全改成es6

--- js/app.js
+++ js/app.js
@@ -1,8 +1,23 @@
 // 这是我们的玩家要躲避的敌人 
-var Enemy = function() {
+//一个方格宽101，高80
+var width = 101;
+var height = 80;
+var delta = 20;
+
+/**
+ * 
+ * @param {*} speed 起始速度
+ * @param {*} row   起始行
+ * @param {*} starPosition 起始位置（-5~-1）避免虫子一窝蜂涌出来
+ */
+var Enemy = function(speed,row,starPosition) {
     // 要应用到每个敌人的实例的变量写在这里
     // 我们已经提供了一个来帮助你实现更多
-
+    //x=0 的话虫子突然出现，有点突兀。
+    this.x = starPosition * width;
+    this.y = height * row - delta;
+    this.speed = speed;
+    this.starPosition = starPosition;
     // 敌人的图片，用一个我们提供的工具函数来轻松的加载文件
     this.sprite = 'images/enemy-bug.png';
 };
@@ -12,6 +27,10 @@ var Enemy = function() {
 Enemy.prototype.update = function(dt) {
     // 你应该给每一次的移动都乘以 dt 参数，以此来保证游戏在所有的电脑上
     // 都是以同样的速度运行的
+    this.x =this.x + this.speed * dt;
+    if(this.x > 5 * width ){
+        this.x = this.starPosition * width;
+    }
 };
 
 // 此为游戏必须的函数，用来在屏幕上画出敌人，
@@ -21,13 +40,76 @@ Enemy.prototype.render = function() {
 
 // 现在实现你自己的玩家类
 // 这个类需要一个 update() 函数， render() 函数和一个 handleInput()函数
-
-
+var Player = function(){
+    this.x = 2 * width;
+    this.y = 4 * height-delta;
+    this.sprite = 'images/char-boy.png'
+}
+Player.prototype.update = function(dt){
+    if(this.x < 0){
+        this.x = 0;
+    }else if(this.x > 4 * width  ){
+        this.x = 4 * width
+    }else if(this.y < 0){
+        alert('恭喜你成功过河啦~~');
+        this.y = 4 * height - delta;
+        creatEnemies();
+    }else if(this.y > 5 * height){
+        this.y = 5 * height - delta
+    }
+}
+Player.prototype.render = function(){
+    ctx.drawImage(Resources.get(this.sprite),this.x,this.y)
+}
+Player.prototype.handleInput = function(key){
+    //根据
+    switch(key){
+        case "left":
+        this.x = this.x - width;
+        break;
+        case "up":
+        this.y = this.y - height;
+        break;
+        case "right":
+        this.x = this.x + width;
+        break;
+        case "down":
+        this.y = this.y + height;
+        break;
+    }
+}
 // 现在实例化你的所有对象
 // 把所有敌人的对象都放进一个叫 allEnemies 的数组里面
 // 把玩家对象放进一个叫 player 的变量里面
+var player = new Player();
+var allEnemies = [];
 
+//从新生成敌人数组
+function creatEnemies(){
+    allEnemies = [];
+    for(var i = 0; i < 6; i++){
+        var row = randomNum(1,3);
+        var speed = randomNum(80,250);
+        var starPosition = randomNum(-5,-1);
+        allEnemies.push(new Enemy(speed,row,starPosition));
+    }
+}
+creatEnemies();
 
+//生成从minNum到maxNum的随机数
+function randomNum(minNum,maxNum){ 
+    switch(arguments.length){ 
+        case 1: 
+            return parseInt(Math.random()*minNum+1,10); 
+        break; 
+        case 2: 
+            return parseInt(Math.random()*(maxNum-minNum+1)+minNum,10); 
+        break; 
+            default: 
+                return 0; 
+            break; 
+    } 
+} 
 // 这段代码监听游戏玩家的键盘点击事件并且代表将按键的关键数字送到 Player.handleInput()
 // 方法里面。你不需要再更改这段代码了。
 document.addEventListener('keyup', function(e) {
--- js/engine.js
+++ js/engine.js
@@ -64,7 +64,7 @@ var Engine = (function(global) {
      */
     function update(dt) {
         updateEntities(dt);
-        // checkCollisions();
+        checkCollisions();
     }
 
     /* 这个函数会遍历在 app.js 定义的存放所有敌人实例的数组，并且调用他们的 update()
@@ -77,7 +77,18 @@ var Engine = (function(global) {
         });
         player.update();
     }
+    /* 这个函数用于判断玩家和敌人的碰撞，如果他们的相撞，玩家就回到原点   */
+    function checkCollisions(){
+        allEnemies.forEach(function(enemy) {
+            if(enemy.y === player.y ){
+                if(enemy.x + width/2 > player.x && enemy.x + width/2 < player.x + width){
+                    player.x = 2 * width;
+                    player.y = 4 * height-delta;
+                }
 
+            }
+        });
+    }
     /* 这个函数做了一些游戏的初始渲染，然后调用 renderEntities 函数。记住，这个函数
      * 在每个游戏的时间间隙都会被调用一次（或者说游戏引擎的每个循环），因为这就是游戏
      * 怎么工作的，他们就像是那种每一页上都画着不同画儿的书，快速翻动的时候就会出现是
